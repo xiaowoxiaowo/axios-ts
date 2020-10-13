b992c74a997f1da85c6497187015a041a3723660
xiaowoxiaowo <awlzluggeoo@sina.cn>
2020-10-13 21:26:41 +0800

    fix: 恢复之前的文件

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -11,3 +11,11 @@ export function isDate(val: any): val is Date {
 export function isPlainObject(val: any): val is Object {
   return toString.call(val) === '[object Object]'
 }
+
+export function extend<T, U>(to: T, from: U): T & U {
+  for (const key in from) {
+    // 以+、-、/、()、[]这些字符开头的语句都需要加分号,因为代码压缩合并后非常容易混淆出错
+    ;(to as T & U)[key] = from[key] as any
+  }
+  return to as T & U
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,21 @@
-export type Method = 'get' | 'GET' | 'post' | 'POST'
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
 
 export interface AxiosRequestConfig {
-  url: string
+  url?: string
   method?: Method
   data?: any
   params?: any
@@ -11,20 +25,44 @@ export interface AxiosRequestConfig {
 }
 
 export interface AxiosResponse {
-  data: any
   status: number
   statusText: string
   headers: any
   config: AxiosRequestConfig
   request: any
+  data: any
 }
 
 export interface AxiosPromise extends Promise<AxiosResponse> {}
 
 export interface AxiosError extends Error {
-  isAxiosError: boolean
   config: AxiosRequestConfig
-  code?: string | null
+  code?: string
   request?: any
   response?: AxiosResponse
+  isAxiosError: boolean
+}
+
+export interface Axios {
+  request(config: AxiosRequestConfig): AxiosPromise
+
+  get(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  delete(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  head(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  options(url: string, config?: AxiosRequestConfig): AxiosPromise
+
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+}
+
+export interface AxiosInstance extends Axios {
+  (config: AxiosRequestConfig): AxiosPromise
+
+  (url: string, config?: AxiosRequestConfig): AxiosPromise
 }
